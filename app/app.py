2bc1cf772f093b807703db56de4d6e277c472eeb
vidhipatel <vkp8.njit.edu>
2021-07-19 10:20:45 -0400

    Task: Updated app.py file

--- app/app.py
+++ app/app.py
@@ -27,11 +27,18 @@ def cities_import() -> List[Dict]:
 
 
 @app.route('/')
-def index() -> str:
+def index():
+    user = {'username': 'Miguel'}
+    cities_data = cities_import()
+
+    return render_template('index.html', title='Home', user=user, cities=cities_data)
+
+
+@app.route('/api/cities')
+def cities() -> str:
     js = json.dumps(cities_import())
     resp = Response(js, status=200, mimetype='application/json')
     return resp
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
